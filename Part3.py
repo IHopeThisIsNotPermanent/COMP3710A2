ed7798f7b1b31b8b616093f736dfeede69c23d11
Saul Rathbone-Boschis <51992820+IHopeThisIsNotPermanent@users.noreply.github.com>
2025-09-11 21:07:05 +1000

    updates playground

--- Part3.py
+++ Part3.py
@@ -1,3 +1,11 @@
+import torch, pickle
 
+def unpickle(file):
+    with open(file, 'rb') as fo:
+        dict = pickle.load(fo, encoding='bytes')
+    return dict
 
-if __name__ == "__main__"
+batch_meta = unpickle('./datasets/cifar-10-batches-py/batches.meta')
+
+if __name__ == "__main__":
+    ...
